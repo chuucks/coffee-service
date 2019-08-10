b6634b2b6e9d17d8140ecf5da0dcb69d37ba9b4f
Carlos Salazar <csalazar@kenzan.com>
2019-08-09 20:18:02 -0500

    Initial commit (project out of Spring initializr)

---
+++ src/main/java/org/codesolt/mongokubernetesdemo/MongoKubernetesDemoApplication.java
@@ -0,0 +1,13 @@
+package org.codesolt.mongokubernetesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoKubernetesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoKubernetesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/codesolt/mongokubernetesdemo/MongoKubernetesDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.codesolt.mongokubernetesdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongoKubernetesDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
